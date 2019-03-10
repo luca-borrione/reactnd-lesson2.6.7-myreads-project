0afb8877a63314d737eb20f6c51631bfcaf9849c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-10 21:26:04 +0000

    Deploy luca-borrione/reactnd-project1-myreads to github.com/luca-borrione/reactnd-project1-myreads.git:gh-pages

---
+++ precache-manifest.4314a73917327996e7996d88a254f9ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project1-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project1-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project1-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "17a975ab8f95ce2790b4",
+    "url": "/reactnd-project1-myreads/static/js/runtime~main.17a975ab.js"
+  },
+  {
+    "revision": "b16cca1d3771a45e273a",
+    "url": "/reactnd-project1-myreads/static/js/main.b16cca1d.chunk.js"
+  },
+  {
+    "revision": "c23c30ab49b6da644885",
+    "url": "/reactnd-project1-myreads/static/js/1.c23c30ab.chunk.js"
+  },
+  {
+    "revision": "b16cca1d3771a45e273a",
+    "url": "/reactnd-project1-myreads/static/css/main.04cb06ad.chunk.css"
+  },
+  {
+    "revision": "202dd8a61c286e89d99395eccb63b2ca",
+    "url": "/reactnd-project1-myreads/index.html"
+  }
+];
